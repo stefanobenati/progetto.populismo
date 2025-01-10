293d57e5117e132c9cdb429ff26ce684e4caa9c1
stefanobenati <stefano.benati@unitn.it>
2025-01-10 12:39:27 +0100

    Add files via upload

---
+++ Dati post scripts/save script environment.R
@@ -0,0 +1,7 @@
+save(auto.calc, file = "autocalcolo smote.Rda")
+save(countries_lista, file = "countries large list.Rda")
+save(count_table_tot, file = "count_table_tot.Rda")
+save(parametri_smote, file = "parametri_smote.Rda")
+save(train, file = "original data training set.Rda")
+save(validation, file = "validation set original.Rda")
+save(stati_bilanciati_train, file = "training smote.Rda")
